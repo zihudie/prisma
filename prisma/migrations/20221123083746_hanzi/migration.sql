06b702f9c02e4d4bfd4886e58ce12b3ba9cfacbd
XIAONIU\wuyaofang <wuyaofang@xiaoniuhy.com>
2022-11-24 15:03:53 +0800

    feat：schema

---
+++ prisma/migrations/20221123083746_hanzi/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Hanzi" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "chinese" TEXT NOT NULL,
+    "pinyin" TEXT NOT NULL,
+    "bihua" TEXT NOT NULL,
+    "radical" TEXT NOT NULL,
+    "fayin" TEXT
+);
